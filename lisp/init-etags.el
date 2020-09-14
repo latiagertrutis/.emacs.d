08c5e5dd54ac8111fb11740e728fb4f48388bb7e
latiagertrutis <30806947+latiagertrutis@users.noreply.github.com>
2020-09-14 14:06:27 +0200

    etags upgrade

--- lisp/init-etags.el
+++ lisp/init-etags.el
@@ -14,16 +14,20 @@
               ("M-g b" . xref-pop-marker-stack)
               ("M-g o" . xref-find-definitions-other-window)))
 
+(setq etags-file "TAGS")
+
 (defun refresh-project-tags ()
   "Rewfres the TAG file of one project."
   (interactive)
-  (let
-      ((tag-dir (locate-dominating-file buffer-file-name tag-file)))
+  (let*
+      ((tag-dir (locate-dominating-file buffer-file-name etags-file))
+       (relative-etags-file (concat (file-name-as-directory tag-dir) etags-file)))
+    (setq tags-table-list `(,tag-dir))
     (if tag-dir
         (shell-command (format
                         "find %s \\( -name \"*.[chCH]\" -o -name \"*.go\" \\) -print | etags - -o %s"
                         tag-dir
-                        "~/.emacs.d/TAGS"))
-      (print (format "No %s file found in project." tag-file)))))
+                        relative-etags-file))
+      (print (format "No %s file found in project." etags-file)))))
 
 (provide 'init-etags)
