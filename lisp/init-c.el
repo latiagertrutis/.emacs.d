4f6b7a84fb89fef3c261f9d2d8faab0b08b59cc7
latiagertrutis <teorodrip@posteo.net>
2021-02-08 12:57:17 +0100

    delete trailing whitespace in c-mode

--- lisp/init-c.el
+++ lisp/init-c.el
@@ -4,7 +4,7 @@
 ;; Author: Mateo Rodriguez Ripolles (teorodrip@posteo.net)
 ;; Maintainer: 
 ;; Created: Thu Jan 28 11:12:29 2021 (+0100)
-;; Last-Updated: Thu Jan 28 12:45:27 2021 (+0100)
+;; Last-Updated: Mon Feb  8 12:56:39 2021 (+0100)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; Insal ccls (language server) client for emacs
@@ -16,6 +16,12 @@
 ;;   (setq-default flycheck-disabled-checkers '(c/c++-clang c/c++-cppcheck c/c++-gcc))
 ;;   :hook
 ;;   (c-mode . (lambda() (require 'ccls) (lsp))))
+
+(defun c-mode-delete-trailing-whitespace ()
+  (when (derived-mode-p 'c-mode)
+    (delete-trailing-whitespace)))
+
+(add-hook 'before-save-hook 'c-mode-delete-trailing-whitespace)
 
 (provide 'init-c)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
