f3bb5ec9caeff290e668cbbcce7fe22fcc69433f
latiagertrutis <teorodrip@posteo.net>
2021-08-09 16:44:35 +0200

    keybind rg

--- lisp/init-utils.el
+++ lisp/init-utils.el
@@ -193,7 +193,7 @@ for the current buffer's file name, and the line number at point."
   (rg-define-search rg-c-mode
     "Search files in a C project"
     :dir (projectile-project-root)
-    :files "*.{c, h, C, H}")
+    :files "*.[chCH]")
   :bind ("C-c r" . rg-c-mode)
   )
 (use-package deadgrep
