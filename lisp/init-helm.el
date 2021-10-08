94fbb5120e7f3836ae9a50635cd59e2059c08e83
Mateo Rodriguez <teorodrip@posteo.net>
2021-10-08 12:13:41 +0200

    helm not diivided

--- lisp/init-helm.el
+++ lisp/init-helm.el
@@ -9,7 +9,7 @@
 (use-package helm
              :ensure t
              :config
-             (setq helm-split-window-in-side-p           t ; open helm buffer inside current window, not occupy whole other window
+             (setq helm-split-window-in-side-p           nil ; open helm buffer inside current window, not occupy whole other window
                    helm-move-to-line-cycle-in-source     nil ; move to end or beginning of source when reaching top or bottom of source.
                    helm-ff-search-library-in-sexp        t ; search for library in `require' and `declare-function' sexp.
                    helm-scroll-amount                    8 ; scroll 8 lines other window using M-<next>/M-<prior>
