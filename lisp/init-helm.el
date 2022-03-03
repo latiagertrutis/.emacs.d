be7aa5e95c7d1a71805258b2075acbf059c3104f
mateorodriguez <teorodrip@gmail.com>
2022-03-03 16:47:24 +0100

    imporved helm completion

--- lisp/init-helm.el
+++ lisp/init-helm.el
@@ -10,7 +10,7 @@
   :ensure t
   :config
   (setq helm-split-window-in-side-p           t ; open helm buffer inside current window, not occupy whole other window
-        helm-move-to-line-cycle-in-source     nil ; move to end or beginning of source when reaching top or bottom of source.
+        helm-move-to-line-cycle-in-source     t ; move to end or beginning of source when reaching top or bottom of source.
         helm-ff-search-library-in-sexp        t ; search for library in `require' and `declare-function' sexp.
         helm-scroll-amount                    8 ; scroll 8 lines other window using M-<next>/M-<prior>
         helm-ff-file-name-history-use-recentf t
@@ -38,7 +38,7 @@
    ("C-x b" . helm-buffers-list)
    ("C-c h" . helm-command-prefix)
    (:map helm-map
-         ("<tab>" . helm-execute-persistent-action)
+         ("<tab>" . helm-next-line)
          ("C-i" . helm-execute-persistent-action)
          ("C-z" . helm-select-action))))
 
