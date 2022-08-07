c0cdd41fccbc69f5749060c9af5de27b350d9bec
mateorodriguez <mateorodriguez@geotab.com>
2022-08-07 16:40:20 +0200

    Some editing utils

--- editing/init-utils.el
+++ editing/init-utils.el
@@ -4,10 +4,32 @@
 ;; Author: Mateo Rodriguez Ripolles (mateorodriguez@geotab.com)
 ;; Maintainer: 
 ;; Created: dom ago  7 14:09:06 2022 (+0200)
-;; Last-Updated: dom ago  7 15:26:20 2022 (+0200)
+;; Last-Updated: dom ago  7 16:39:09 2022 (+0200)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (require 'bind-key)
+
+;;----------------------------------------------------------------------------
+;; Some basic preferences
+;;----------------------------------------------------------------------------
+(setq-default
+ blink-cursor-interval 0.4
+ bookmark-default-file (expand-file-name ".bookmarks.el" user-emacs-directory)
+ buffers-menu-max-size 30
+ case-fold-search t
+ column-number-mode t
+ delete-selection-mode t
+ ediff-split-window-function 'split-window-horizontally
+ ediff-window-setup-function 'ediff-setup-windows-plain
+ indent-tabs-mode t
+ make-backup-files nil
+ mouse-yank-at-point t
+ save-interprogram-paste-before-kill t
+ scroll-preserve-screen-position 'always
+ set-mark-command-repeat-pop t
+ tooltip-delay 1.5
+ truncate-lines nil
+ truncate-partial-width-windows nil)
 
 ;;----------------------------------------------------------------------------
 ;; Delete the current file
@@ -170,6 +192,43 @@ This command does not push text to `kill-ring'."
 
 (bind-key* "M-d" 'my-delete-word)
 (bind-key* "<C-backspace>" 'my-backward-delete-word)
+
+(use-package whole-line-or-region
+             :ensure t
+             :hook
+             (after-init . whole-line-or-region-global-mode)
+             :bind
+             ("C-M-w" . whole-line-or-region-delete-region))
+
+(use-package smooth-scrolling
+             :ensure t
+             :config
+             (smooth-scrolling-mode 1))
+
+(when (fboundp 'electric-pair-mode)
+  (add-hook 'after-init-hook 'electric-pair-mode))
+
+(defun sanityinc/newline-at-end-of-line ()
+  "Move to end of line, enter a newline, and reindent."
+  (interactive)
+  (move-end-of-line 1)
+  (newline-and-indent))
+
+(bind-key* "S-<return>" 'sanityinc/newline-at-end-of-line)
+
+(use-package rainbow-delimiters
+             :ensure t
+             :hook
+             (prog-mode . rainbow-delimiters-mode))
+
+(use-package avy
+             :ensure t
+             :config
+             (global-set-key (kbd "C-o") 'avy-goto-char-timer))
+
+(when (fboundp 'display-line-numbers-mode)
+  (setq-default display-line-numbers-width 3)
+  (add-hook 'prog-mode-hook 'display-line-numbers-mode))
 
 (provide 'init-utils)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
