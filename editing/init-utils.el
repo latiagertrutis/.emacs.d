69dd0d19e2f22fd1a7f5af49a24d83b16db65929
mateorodriguez <mateorodriguez@geotab.com>
2022-08-08 17:29:25 +0200

    Added delete-selection mode

--- editing/init-utils.el
+++ editing/init-utils.el
@@ -18,7 +18,6 @@
  buffers-menu-max-size 30
  case-fold-search t
  column-number-mode t
- delete-selection-mode t
  ediff-split-window-function 'split-window-horizontally
  ediff-window-setup-function 'ediff-setup-windows-plain
  indent-tabs-mode t
@@ -30,6 +29,9 @@
  tooltip-delay 1.5
  truncate-lines nil
  truncate-partial-width-windows nil)
+
+(when (fboundp 'delete-selection-mode)
+  (add-hook 'after-init-hook 'delete-selection-mode))
 
 ;;----------------------------------------------------------------------------
 ;; Delete the current file
