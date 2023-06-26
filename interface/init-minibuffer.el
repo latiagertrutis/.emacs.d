f52dee7dcc91141efabf2bccff173e0b82452ddc
mateorodriguez <mateorodriguez@geotab.com>
2023-06-26 16:30:54 +0200

    Enhanced projectile set up

--- interface/init-minibuffer.el
+++ interface/init-minibuffer.el
@@ -4,7 +4,7 @@
 ;; Author: Mateo Rodriguez Ripolles (mateorodriguez@geotab.com)
 ;; Maintainer: 
 ;; Created: sáb ago  6 21:06:24 2022 (+0200)
-;; Last-Updated: mié abr 19 15:12:01 2023 (+0200)
+;; Last-Updated: lun jun 26 16:28:44 2023 (+0200)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defun my-filter-dired-buffers (buffer-list)
@@ -46,7 +46,7 @@
   :bind
   (("M-x" . helm-M-x)
    ("C-x C-f" . helm-find-files)
-   ("C-x b" . helm-buffers-list)
+   ("C-x C-b" . helm-buffers-list)
    ("C-c h" . helm-command-prefix)
    (:map helm-map
          ("<tab>" . helm-execute-persistent-action)
--- project_management/init-project-management.el
+++ project_management/init-project-management.el
@@ -4,8 +4,14 @@
 ;; Author: Mateo Rodriguez Ripolles (mateorodriguez@geotab.com)
 ;; Maintainer: 
 ;; Created: dom ago  7 14:43:21 2022 (+0200)
-;; Last-Updated: dom ago  7 14:45:23 2022 (+0200)
+;; Last-Updated: lun jun 26 16:30:10 2023 (+0200)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defun my-generate-projects-dirs (root-workspace)
+  (setq projects-list '())
+  (dolist (dir (directory-files root-workspace t "Sprint_.*"))
+    (setq projects-list (append projects-list (directory-files dir t "EMB-.*"))))
+  projects-list)
 
 (use-package projectile
              :ensure t
@@ -15,15 +21,15 @@
              (projectile-mode +1)
              (setq projectile-indexing-method 'alien
                    projectile-project-root-files #'(".projectile")
+		   projectile-project-search-path (my-generate-projects-dirs "~/workspace")
                    projectile-project-root-files-functions #'(projectile-root-top-down
                                                               projectile-root-top-down-recurring
                                                               projectile-root-bottom-up
                                                               projectile-root-local))
-             (projectile-register-project-type 'c '(".cproject")
-                                               :project-file ".cproject")
              :bind
              (:map projectile-mode-map
                    ("s-p" . projectile-command-map)
+                   ("C-x b" . helm-projectile-switch-to-buffer)
                    ("C-c p" . projectile-command-map))
              :hook
              (after-init . projectile-mode))
