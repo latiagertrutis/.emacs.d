5056ce5b5bf2b13a0d254e2344fe7ba06d92a3d0
Mateo <teorodrip@posteo.net>
2024-12-14 17:13:16 +0100

    Fix go mode error

--- programming/init-go.el
+++ programming/init-go.el
@@ -4,7 +4,7 @@
 ;; Author: Mateo Rodriguez Ripolles (mateorodriguez@geotab.com)
 ;; Maintainer: 
 ;; Created: dom ago  7 15:01:38 2022 (+0200)
-;; Last-Updated: dom ago  7 15:01:44 2022 (+0200)
+;; Last-Updated: Sat Dec 14 17:09:10 2024 (+0100)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (use-package go-mode
@@ -13,14 +13,14 @@
   ; Use goimports instead of go-fmt
   (setq gofmt-command "goimports")
   ;; Call Gofmt before saving
-  (add-hook 'before-save-hook 'gofmt-before-save)
+  (add-hook 'before-save-hook #'gofmt-before-save)
   (add-hook 'go-mode-hook #'flycheck-mode)
   (add-hook 'go-mode-hook #'yas-minor-mode)
-  (add-hook 'go-mode-hook '(lambda ()
+  (add-hook 'go-mode-hook (lambda ()
 			     (local-set-key (kbd "C-c C-r") 'go-remove-unused-imports)))
-  (add-hook 'go-mode-hook '(lambda ()
+  (add-hook 'go-mode-hook (lambda ()
 			     (local-set-key (kbd "C-c C-g") 'go-goto-imports)))
-  (add-hook 'go-mode-hook '(lambda ()
+  (add-hook 'go-mode-hook (lambda ()
 			     (setq tab-width 4)))
   (add-hook 'go-mode-hook (lambda ()
 			    (set (make-local-variable 'company-backends) '(company-go))
