d7ac38e31f34279763af036f9be2c58360892c9f
mateo <teorodrip@posteo.net>
2025-03-02 22:10:57 +0100

    Changed to Nord theme

--- interface/init-theme.el
+++ interface/init-theme.el
@@ -4,13 +4,17 @@
 ;; Author: Mateo Rodriguez Ripolles (mateorodriguez@geotab.com)
 ;; Maintainer: 
 ;; Created: sáb ago  6 21:29:26 2022 (+0200)
-;; Last-Updated: dom ago  7 15:23:02 2022 (+0200)
+;; Last-Updated: Sun Mar  2 21:47:44 2025 (+0100)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(use-package zenburn-theme
+(use-package doom-themes
   :ensure t
   :config
-  (load-theme 'zenburn t))
+  (load-theme 'doom-nord t)
+  :custom-face
+  (hl-line ((t (:background "#333A47"))))  
+  )
+
 
 (provide 'init-theme)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
