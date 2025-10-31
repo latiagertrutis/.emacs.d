a256a17f801929231d73765d74db259040a9938d
latiagertrutis <teorodrip@posteo.net>
2025-10-31 19:47:04 +0100

    Fix two frontends showing

--- programming/init-lsp.el
+++ programming/init-lsp.el
@@ -61,26 +61,7 @@
   :custom-face
   (company-preview ((nil(:background "dark slate blue"))))
   :config
-  (define-key company-active-map (kbd "TAB") 'company-complete-selection)
-  (define-key company-active-map (kbd "<tab>") 'company-complete-selection)
-  (define-key company-active-map [return] 'company-complete-selection)
-  (define-key company-active-map (kbd "RET") 'company-complete-selection)
-  ;; auto-complete compatibility
-  (defun my-company-visible-and-explicit-action-p ()
-    (and (company-tooltip-visible-p)
-         (company-explicit-action-p)))
-  (defun company-ac-setup ()
-    "Sets up `company-mode' to behave similarly to `auto-complete-mode'."
-    (setq company-require-match nil)
-    (setq company-auto-complete #'my-company-visible-and-explicit-action-p)
-    (setq company-frontends '(company-echo-metadata-frontend
-                              company-pseudo-tooltip-unless-just-one-frontend-with-delay
-                              company-preview-frontend))
-    (define-key company-active-map [tab]
-		'company-select-next-if-tooltip-visible-or-complete-selection)
-    (define-key company-active-map (kbd "TAB")
-		'company-select-next-if-tooltip-visible-or-complete-selection))
-  (company-ac-setup))
+  (setq company-frontends '(company-preview-frontend)))
 
 ;; Better forntend for company
 (use-package company-box  
