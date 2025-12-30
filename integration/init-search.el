7edea6d07f9d3013cffbd9341be017cea6672230
latiagertrutis <teorodrip@posteo.net>
2025-12-30 15:22:31 +0100

    Improve default directory

--- integration/init-search.el
+++ integration/init-search.el
@@ -4,22 +4,30 @@
 ;; Author: Mateo Rodriguez Ripolles (mateorodriguez@geotab.com)
 ;; Maintainer: 
 ;; Created: dom ago  7 14:30:37 2022 (+0200)
-;; Last-Updated: Thu Jun 19 13:24:11 2025 (+0200)
+;; Last-Updated: Tue Dec 30 15:21:37 2025 (+0100)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;;----------------------------------------------------------------------------
 ;; Better grep
 ;;----------------------------------------------------------------------------
+
+(defun my/project-root-or-default ()
+  (or (projectile-project-root)
+      (magit-toplevel)
+      (expand-file-name default-directory)
+      ((expand-file-name "~")))
+  )
+
 (use-package rg
   :ensure t
   :config
   (rg-define-search rg-c-mode
     "Search files in a C project"
-    :dir (projectile-project-root)
+    :dir (my/project-root-or-default)
     :files "*.[chCH]")
   (rg-define-search rg-all-mode
     "Search files of all types in a C project"
-    :dir (projectile-project-root)
+    :dir (my/project-root-or-default)
     :files "*")
   (setq rg-command-line-flags '("--sort path" "--hidden"))
   :bind
