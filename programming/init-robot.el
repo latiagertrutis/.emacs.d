564d425722312e33bd1ab262896b296429217a30
latiagertrutis <teorodrip@posteo.net>
2026-01-23 19:08:10 +0100

    Add after save format on robot

--- programming/init-robot.el
+++ programming/init-robot.el
@@ -1,14 +1,23 @@
 ;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Filename: init-robot.el
-;; Description: 
+;; Description:
 ;; Author: Mateo Rodriguez Ripolles (teorodrip@posteo.net)
-;; Maintainer: 
+;; Maintainer:
 ;; Created: Mon Apr 21 19:26:13 2025 (+0200)
-;; Last-Updated: Mon Apr 21 19:27:17 2025 (+0200)
+;; Last-Updated: Fri Jan 23 19:07:16 2026 (+0100)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+(defun my-robot-mode-format ()
+  (interactive)
+  ;; Call shell-command-to-string to discard output
+  (shell-command-to-string (format "robocop format %s" buffer-file-name))
+  (revert-buffer nil 't))
+
 (use-package robot-mode
-  :ensure t)
+  :ensure t
+  :hook
+  ;; https://emacs.stackexchange.com/questions/5452/before-save-hook-for-cc-mode
+  (robot-mode . (lambda () (add-hook 'after-save-hook #'my-robot-mode-format nil 'local))))
 
 (provide 'init-robot)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
