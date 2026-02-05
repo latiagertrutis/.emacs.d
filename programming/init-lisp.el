625aeb887f42aa604ed2d41b9361a71d15a821c4
latiagertrutis <teorodrip@posteo.net>
2026-02-05 14:19:15 +0100

    Add shortkeys for elisp

--- programming/init-lisp.el
+++ programming/init-lisp.el
@@ -4,10 +4,14 @@
 ;; Author: Mateo Rodriguez Ripolles (teorodrip@posteo.net)
 ;; Maintainer: 
 ;; Created: vie sep  2 15:53:53 2022 (+0200)
-;; Last-Updated: vie sep  2 15:56:18 2022 (+0200)
+;; Last-Updated: Thu Feb  5 13:58:23 2026 (+0100)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(add-hook 'emacs-lisp-mode-hook 'company-mode)
+(add-hook 'emacs-lisp-mode-hook #'company-mode)
+
+(keymap-set emacs-lisp-mode-map "M-g d" #'xref-find-definitions)
+(keymap-set emacs-lisp-mode-map "M-g o" #'xref-find-definitions-other-window)
+(keymap-set emacs-lisp-mode-map "M-g r" #'xref-find-references)
 
 (provide 'init-lisp)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
