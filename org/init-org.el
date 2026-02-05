e47289b0aca26f17f4235e70f71babe1958654eb
latiagertrutis <teorodrip@posteo.net>
2026-02-05 21:41:51 +0100

    remove org junk && remap org shift to no arrows

--- org/init-org.el
+++ org/init-org.el
@@ -4,7 +4,7 @@
 ;; Author: Mateo Rodriguez Ripolles (teorodrip@posteo.net)
 ;; Maintainer: 
 ;; Created: mar sep  6 11:45:11 2022 (+0200)
-;; Last-Updated: Thu Feb  5 17:27:42 2026 (+0100)
+;; Last-Updated: Thu Feb  5 21:41:27 2026 (+0100)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; Org roam
@@ -19,132 +19,10 @@
   ("C-c n i" . org-roam-node-insert)
   ("C-c n f" . org-roam-node-find)
   ("C-c n c" . org-roam-capture)
-)
+  )
 
-(global-set-key (kbd "C-c a") 'org-agenda)
-(global-set-key (kbd "C-c c") 'org-capture)
-
-;; Default directories
-(setq org-directory (expand-file-name "~/org"))
-(setq org-default-notes-file (concat org-directory "/notes.org"))
-(setq org-agenda-files (concat org-directory "/agenda/"))
-
-(dolist (f (list org-directory))
-  (unless (file-exists-p f)
-    (make-directory f)))
-
-(defun my/new-note ()
-  (let ((fpath (read-file-name "Project file name: "
-                               (concat org-directory "/notes/")
-                               nil nil nil)))
-    (find-file fpath)
-    (goto-char (point-min))))
-
-;; Capture templates
-(setq org-capture-templates
-      '(("t" "Todo" entry (file+headline org-default-notes-file "Tasks")
-         "* TODO %?\n  %T\n  %i")
-        ("j" "Journal" entry (file+datetree org-default-notes-file)
-         "* %?\n  %T\n  %i")
-	("n" "New note in Inbox" plain
-         (function my/new-note))
-	("a" "Append Journal" plain (file org-default-notes-file)
-	 "%?\n  %i")))
-
-(setq org-agenda-custom-commands
-      '(("n" "Non TODO entries"
-	 ((agenda ""
-		  ((org-agenda-span 15)
-		   (org-agenda-start-day "-14d")
-		   (org-agenda-entry-types '(:timestamp))))))))
-
-;; Define TODO and TAG keywords with smooth colors.
-(setq org-todo-keywords
-      '(
-        (sequence "IDEA(i)" "TODO(t)" "STARTED(s)" "NEXT(n)" "WAITING(w)" "|" "DONE(d)")
-        (sequence "|" "CANCELED(c)" "DELEGATED(l)" "SOMEDAY(f)")
-        ))
-
-(setq org-todo-keyword-faces
-      '(("IDEA" . (:foreground "GoldenRod" :weight bold))
-        ("NEXT" . (:foreground "IndianRed1" :weight bold))
-        ("STARTED" . (:foreground "OrangeRed" :weight bold))
-        ("WAITING" . (:foreground "coral" :weight bold))
-        ("CANCELED" . (:foreground "LimeGreen" :weight bold))
-        ("DELEGATED" . (:foreground "LimeGreen" :weight bold))
-        ("SOMEDAY" . (:foreground "LimeGreen" :weight bold))
-        ))
-
-(setq org-tag-persistent-alist
-      '((:startgroup . nil)
-        ("HOME" . ?h)
-        ("RESEARCH" . ?r)
-        ("TEACHING" . ?t)
-        (:endgroup . nil)
-        (:startgroup . nil)
-        ("OS" . ?o)
-        ("DEV" . ?d)
-        ("WWW" . ?w)
-        (:endgroup . nil)
-        (:startgroup . nil)
-        ("EASY" . ?e)
-        ("MEDIUM" . ?m)
-        ("HARD" . ?a)
-        (:endgroup . nil)
-        ("UCANCODE" . ?c)
-        ("URGENT" . ?u)
-        ("KEY" . ?k)
-        ("BONUS" . ?b)
-        ("noexport" . ?x)
-        )
-      )
-
-(setq org-tag-faces
-      '(
-        ("HOME" . (:foreground "GoldenRod" :weight bold))
-        ("RESEARCH" . (:foreground "GoldenRod" :weight bold))
-        ("TEACHING" . (:foreground "GoldenRod" :weight bold))
-        ("OS" . (:foreground "IndianRed1" :weight bold))
-        ("DEV" . (:foreground "IndianRed1" :weight bold))
-        ("WWW" . (:foreground "IndianRed1" :weight bold))
-        ("URGENT" . (:foreground "Red" :weight bold))
-        ("KEY" . (:foreground "Red" :weight bold))
-        ("EASY" . (:foreground "OrangeRed" :weight bold))
-        ("MEDIUM" . (:foreground "OrangeRed" :weight bold))
-        ("HARD" . (:foreground "OrangeRed" :weight bold))
-        ("BONUS" . (:foreground "GoldenRod" :weight bold))
-        ("UCANCODE" . (:foreground "GoldenRod" :weight bold))
-        ("noexport" . (:foreground "LimeGreen" :weight bold))
-        )
-      )
-
-(setq org-fast-tag-selection-single-key t)
-(setq org-use-fast-todo-selection t)
-(setq org-use-speed-commands t)
-
-(org-babel-do-load-languages
- 'org-babel-load-languages
- '(
-   (emacs-lisp . t)
-   (org . t)
-   (shell . t)
-   (C . t)
-   (python . t)
-   (gnuplot . t)
-   (octave . t)
-   (R . t)
-   (dot . t)
-   (awk . t)
-   ))
-
-(setq org-src-fontify-natively t)
-(setq org-src-tab-acts-natively t)
-
-(add-to-list 'ispell-skip-region-alist '(":\\(PROPERTIES\\|LOGBOOK\\):" . ":END:"))
-(add-to-list 'ispell-skip-region-alist '("#\\+BEGIN_SRC" . "#\\+END_SRC"))
-(add-to-list 'ispell-skip-region-alist '("#\\+BEGIN_EXAMPLE" . "#\\+END_EXAMPLE"))
-
-(add-hook 'org-mode-hook (lambda () (setq truncate-lines nil)))
+(keymap-set org-mode-map "C-;" #'org-shiftleft)
+(keymap-set org-mode-map "C-'" #'org-shiftright)
 
 (provide 'init-org)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
