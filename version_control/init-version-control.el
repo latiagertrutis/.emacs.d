b298772712b8166db095e833ae268f69d4114a56
latiagertrutis <teorodrip@posteo.net>
2026-02-24 14:11:01 +0100

    bind magit find file

--- version_control/init-version-control.el
+++ version_control/init-version-control.el
@@ -4,7 +4,7 @@
 ;; Author: Mateo Rodriguez Ripolles (mateorodriguez@geotab.com)
 ;; Maintainer: 
 ;; Created: dom ago  7 15:19:00 2022 (+0200)
-;; Last-Updated: dom ago  7 15:19:21 2022 (+0200)
+;; Last-Updated: Tue Feb 24 14:10:33 2026 (+0100)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (use-package magit
@@ -16,7 +16,8 @@
   (setq-default magit-diff-refine-hunk t)
   (fullframe magit-status magit-mode-quit-window)
   :bind
-  ("C-x g" . magit-status))
+  ("C-x g" . magit-status)
+  ("C-x f" . magit-find-file))
 
 (provide 'init-version-control)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
