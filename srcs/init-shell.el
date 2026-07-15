def7f7a1ddee9059097c83415c49553503fe4387
latiagertrutis <teorodrip@posteo.net>
2026-07-15 18:16:29 +0200

    Add sql mode format

--- srcs/init-shell.el
+++ srcs/init-shell.el
@@ -9,4 +9,12 @@
   (nushell-mode . lsp)
   (nushell-mode . (lambda () (add-hook 'after-save-hook #'my-nushell-mode-format nil 'local))))
 
+(defun my-postgres-mode-format ()
+  (interactive)
+  ;; Call shell-command-to-string to discard output
+  (shell-command-to-string (format "pg_format -i %s" buffer-file-name))
+  (revert-buffer nil 't))
+
+(add-hook 'sql-mode-hook (lambda () (add-hook 'after-save-hook #'my-postgres-mode-format nil 'local)))
+
 (provide 'init-shell)
